d8e57568aed2931dfc884d83b64310d7a326aa16
vladislav malygin <malyginvlad6220@gmail.com>
2025-01-07 05:52:37 +0300

    setup flake8 cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,8 +73,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
-
 
 
 # Password validation
